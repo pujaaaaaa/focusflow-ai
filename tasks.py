7a4ab6278cb9998ce2d440ef4d89cba661ce998b
pujaaaaaa <69100637+pujaaaaaa@users.noreply.github.com>
2026-04-08 18:23:14 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,1 +1,14 @@
-TASKS = ["Email", "Coding", "Meeting", "Design"]
+TASKS = [
+    "Deep Work",
+    "Coding",
+    "Debugging",
+    "Meetings",
+    "Emails",
+    "Planning",
+    "Research",
+    "Learning",
+    "Code Review",
+    "Documentation",
+    "Creative Thinking",
+    "Break / Recharge"
+]
